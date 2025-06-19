a4c4c3b8731c2ad58dd32c6139c1ebe81fd06c47
lgbouma <bouma.luke@gmail.com>
2025-06-19 11:39:23 -0700

    lc_processing: add time_bin_lightcurve

--- pbls/lc_processing.py
+++ pbls/lc_processing.py
@@ -1,3 +1,4 @@
+import numpy as np
 from astropy.timeseries import LombScargle
 
 def get_LS_Prot(time, flux, Prot_min=0.1, Prot_max=15., N_freq = 1_000_000):
@@ -14,3 +15,26 @@ def get_LS_Prot(time, flux, Prot_min=0.1, Prot_max=15., N_freq = 1_000_000):
     print(f"Measured LS period: {LS_Prot:.4f} days")
 
     return LS_Prot
+
+def time_bin_lightcurve(time, flux, binsize):
+    """Bin the light curve in time with a fixed binsize.
+    Returns arrays of binned time (bin centers) and mean flux in each bin."""
+
+    t_min, t_max = np.nanmin(time), np.nanmax(time)
+    # Define bin edges from t_min to t_max
+    edges = np.arange(t_min, t_max + binsize, binsize)
+    # Digitize assigns each time to a bin index
+    bin_idx = np.digitize(time, edges) - 1
+
+    binned_time = []
+    binned_flux = []
+    # Iterate over all bins
+    for i in range(len(edges) - 1):
+        mask = bin_idx == i
+        if not np.any(mask):
+            continue
+        # Compute mean time and flux in the bin
+        binned_time.append(np.nanmean(time[mask]))
+        binned_flux.append(np.nanmean(flux[mask]))
+
+    return np.array(binned_time), np.array(binned_flux)
