17d7f2d5e5ec635bfaae8b260d7005e7c2de7761
lgbouma <bouma.luke@gmail.com>
2025-06-23 17:56:30 -0500

    add LICENSE, requirements, package script

---
+++ _package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# define the py311 venv from scratch; use the latest pbls version on disk
+cd ../ ;
+tar czf pbls-0.0.0.tar.gz pbls ;
+mv pbls-0.0.0.tar.gz $HOME/environments/ ;
+cd $HOME/environments/ ;
+
+# NOTE: one dependency is not included in this auto-updated: the
+# complexrotators package, https://github.com/lgbouma/cpv, because I have not
+# put it on pip yet.
+pip3 install --target=$HOME/environments/py311 -r requirements.txt ;
+pip3 install --target=$HOME/environments/py311 --upgrade pbls-0.0.0.tar.gz ;
+rm pbls-0.0.0.tar.gz ;
+
+# move tarballed environment to runtime directory for exporting on OSG
+cd $HOME/environments;
+tar czf py311.tar.gz py311
+
+cd $HOME/proj/pbls/drivers
+mv $HOME/environments/py311.tar.gz .
