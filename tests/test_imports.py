8d16324c049408d3ebb6589a2281e3412eef2739
lgbouma <bouma.luke@gmail.com>
2025-06-23 18:03:49 -0500

    add test_imports

---
+++ tests/test_imports.py
@@ -0,0 +1,20 @@
+def test_imports():
+
+    modules = ['pbls', 'numpy', 'numba', 'wotan', 'astropy', 'pandas']
+
+    for m in modules:
+
+        dep_worked = True
+
+        try:
+            exec(f"import {m}")
+            print(f"'import {m}' passed.")
+            dep_worked = True
+        except Exception as e:
+            print(e)
+            dep_worked = False
+
+        assert dep_worked
+
+if __name__ == "__main__":
+    test_imports()
