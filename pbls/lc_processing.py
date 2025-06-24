16272d0a7438cab42afc54598155f21a7379e21a
lgbouma <bouma.luke@gmail.com>
2025-06-24 12:31:50 -0500

    lc_processing: add preprocess_lightcurve

--- pbls/lc_processing.py
+++ pbls/lc_processing.py
@@ -1,5 +1,14 @@
+"""
+Standard light curve processing functions.
+
+Contents:
+* preprocess_lightcurve: Standard cleaning before PBLS.
+* get_LS_Prot: Measure rotation period via Lomb-Scargle peak given time and flux.
+* time_bin_lightcurve: Bin the light curve in time with a fixed binsize.
+"""
 import numpy as np
 from astropy.timeseries import LombScargle
+from wotan import slide_clip
 
 def get_LS_Prot(time, flux, Prot_min=0.1, Prot_max=15., N_freq = 1_000_000):
     """measure rotation period (via Lomb Scargle peak) from the light curve"""
@@ -38,3 +47,79 @@ def time_bin_lightcurve(time, flux, binsize):
         binned_flux.append(np.nanmean(flux[mask]))
 
     return np.array(binned_time), np.array(binned_flux)
+
+def preprocess_lightcurve(datas, hdrs, mission):
+    """
+    Drop non-zero quality flags; require finite time and flux;
+    require positive flux; set dtype to float64; median normalize
+    sliding clip [100,2]*MAD over LS_Prot/20 to trim flares;
+    re-require finite time and flux.
+    The flare window requires at least 10 points per window
+    -> K2/Kepler means at least 5 hours window;
+    TESS means at least 20 minute window;
+    but standard window is 10% of LS_Prot.
+    """
+
+    _time, _flux = [], []
+    sectors = []
+
+    for data, hdr in zip(datas, hdrs):
+
+        time = data['TIME']
+
+        # NOTE: logic here could be better; could drop `mission` arg and read
+        # direct from header.
+        if mission == 'TESS':
+            flux = data['SAP_FLUX']
+            qual = data['QUALITY']
+        elif mission == 'K2':
+            flux = data['FCOR'] # EVEREST corrected flux
+            qual = np.zeros_like(time) # K2 data doesn't have QUALITY column
+        elif mission == 'Kepler':
+            flux = data['SAP_FLUX']
+            qual = data['SAP_QUALITY']
+        
+        sel = (qual == 0)
+        time = time[sel]
+        flux = flux[sel]
+
+        sel = np.isfinite(time) & np.isfinite(flux)
+        time = time[sel]
+        flux = flux[sel]
+
+        sel = (flux > 0)
+        time = time[sel]
+        flux = flux[sel]
+
+        time = time.astype(np.float64)
+        flux = flux.astype(np.float64)
+        
+        flux /= np.nanmedian(flux)
+
+        LS_Prot = get_LS_Prot(time, flux)
+        cadence = np.nanmedian(np.diff(time))
+        window_length = np.maximum(LS_Prot/20, cadence * 10)
+
+        clipped_flux = slide_clip(
+            time, flux, window_length=window_length,
+            low=100, high=2, method='mad', center='median'
+        )
+
+        sel = np.isfinite(time) & np.isfinite(clipped_flux)
+        time = time[sel]
+        flux = 1.* clipped_flux[sel]
+        assert len(time) == len(flux)
+
+        # run TESS analysis at 30-minute binning after flare removal
+        if mission == 'TESS':
+            binsize = 30/24/60
+            btimes, bfluxs = time_bin_lightcurve( time, flux, binsize=binsize )
+            time, flux = 1.*btimes, 1.*bfluxs
+
+        _time.append(time)
+        _flux.append(flux)
+
+    time = np.concatenate(_time)
+    flux = np.concatenate(_flux)
+
+    return time, flux
