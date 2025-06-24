f945a02747e41e11b6b46ccbce4a167063b4b91c
lgbouma <bouma.luke@gmail.com>
2025-06-23 19:26:56 -0500

    paths: simplify

--- pbls/paths.py
+++ pbls/paths.py
@@ -1,33 +1,9 @@
-"""
-This module contains paths that are re-used throughout the project.  Crucially,
-it makes a hidden local cache directory, at ~/.pbls_cache, where results from
-large batch runs will by default be stored.
-"""
-import os
-from os.path import join
+import os, socket
 from pbls import __path__
-__path__ = list(__path__)
+from os.path import join
 
-RESULTSDIR = join(os.path.dirname(__path__[0]), 'results')
+VERBOSE = 0
 
-TESTRESULTSDIR = join(RESULTSDIR, 'tests')
-
-TABLEDIR = join(RESULTSDIR, 'tables')
-
-CACHEDIR = join(os.path.expanduser('~'), '.pbls_cache')
-
-PAPERDIR = join(os.path.dirname(__path__[0]), 'papers', 'paper')
-
-DATADIR = join(__path__[0], 'data')
-
-SECRETDATADIR = join(os.path.dirname(__path__[0]), 'secret_data')
-
-for l in [DATADIR, CACHEDIR, RESULTSDIR, TABLEDIR, TESTRESULTSDIR]:
-    if not os.path.exists(l):
-        print(f"Making {l}")
-        os.mkdir(l)
-
-# used for making non-reusable functionality
-LOCALDIR = join(os.path.expanduser('~'), 'local')
-if not os.path.exists(LOCALDIR):
-    print(f"Did not find {l}; continuing anyway.")
+# cache for temporary files
+CACHEDIR = join(os.path.expanduser("~"), ".pbls_cache")
+if not os.path.exists(CACHEDIR): os.mkdir(CACHEDIR)
