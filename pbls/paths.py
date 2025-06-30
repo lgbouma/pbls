a8e5f2b2788498bfdb52f8cf274a9df832e5863f
lgbouma <bouma.luke@gmail.com>
2025-06-30 10:15:22 -0700

    paths: add datadir

--- pbls/paths.py
+++ pbls/paths.py
@@ -7,3 +7,6 @@ VERBOSE = 0
 # cache for temporary files
 CACHEDIR = join(os.path.expanduser("~"), ".pbls_cache")
 if not os.path.exists(CACHEDIR): os.mkdir(CACHEDIR)
+
+# data storage directory
+DATADIR = os.path.join(os.path.dirname(__path__[0]), 'data')
