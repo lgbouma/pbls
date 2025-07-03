b6a7275615a2b7ecc8b9e88cd12461c5e47e8fba
lgbouma <bouma.luke@gmail.com>
2025-07-03 16:18:54 -0500

    chunk_processing: Stage updated "tarball joiner"
    (Relevant really only for merging on whN systems and laptops.)

--- pbls/chunk_processing.py
+++ pbls/chunk_processing.py
@@ -4,33 +4,51 @@ Contents:
 """
 from pbls.pipeline_utils import extract_tarball
 from glob import glob
-import os, pickle
+import os, pickle, socket
 from os.path import join
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
-from aesthetic.plot import set_style
 from pbls.visualization import plot_periodogram
 
-def join_tarball_chunks_to_periodogram(star_id):
+def join_tarball_chunks_to_periodogram(star_id, iter_ix=0):
+    """
+    NOTE: this function is probably deprecated by drivers/run_pbls_merge.py.
+    """
+    raise DeprecationWarning(
+        "This function is deprecated. "
+        "Please use drivers/run_pbls_merge.py instead."
+    )
 
-    receivingdir = f'/ar0/RECEIVING/{star_id}'
-    tar_paths = glob(join(receivingdir, f'joboutput_{star_id}*.tar.gz'))
+    hostname = socket.gethostname()
+
+    if hostname in ['wh1', 'wh2', 'wh3']:
+        receivingdir = f'/ar0/RECEIVING/{star_id}'
+        processingdir = f'/ar0/PROCESSING/{star_id}'
+        outprocessingdir = f'/ar0/PROCESSING/merged_periodograms'
+    elif 'osg' in hostname:
+        receivingdir = f'/ospool/ap21/data/ekul/pbls_results/{star_id}'
+        processingdir = f'/ospool/ap21/data/ekul/pbls_results/PROCESSING/{star_id}'
+        outprocessingdir = f'/ospool/ap21/data/ekul/pbls_results/PROCESSING/merged_periodograms'
+    else:
+        raise NotImplementedError(
+            f"Hostname {hostname} not recognized. "
+            "Please implement the logic for this hostname."
+        )
+
+    if not os.path.exists(processingdir):
+        os.mkdir(processingdir)
+    if not os.path.exists(outprocessingdir):
+        os.mkdir(outprocessingdir)
+
+    tar_paths = glob(join(receivingdir, f'joboutput_{star_id}*iter{iter_ix}.tar.gz'))
     N_tars = len(tar_paths)
     print(f'Found {N_tars} tarballs for {star_id} in {receivingdir}')
-
-    processingdir = f'/ar0/PROCESSING/{star_id}'
-    if not os.path.exists(processingdir):
-        os.makedirs(processingdir)
-
-    outprocessingdir = f'/ar0/PROCESSING/merged_periodograms'
-    if not os.path.exists(outprocessingdir):
-        os.makedirs(outprocessingdir)
 
     for ix, tar_path in enumerate(tar_paths):
         if ix % int(N_tars/10) == 0:
             print(f"{ix}/{N_tars}...")
         extract_tarball(tar_path, processingdir, verbose=0)
 
-    pklpaths = np.sort(glob(join(processingdir, 'srv', '*', f'{star_id}_*.pkl')))
+    pklpaths = np.sort(glob(join(processingdir, 'srv', '*', f'{star_id}*iter{iter_ix}.pkl')))
 
     powers = []
     periods = []
@@ -80,17 +98,16 @@ def join_tarball_chunks_to_periodogram(star_id):
     }
 
     outdf = pd.DataFrame({'period': periods, 'power': powers})
-    outcsv = join(outprocessingdir, f'{star_id}_merged_pbls_periodogram.csv')
+    outcsv = join(outprocessingdir, f'{star_id}_merged_pbls_periodogram_iter{iter_ix}.csv')
     outdf.to_csv(outcsv, index=False)
     print(f"Wrote merged periodogram to {outcsv}")
 
-    outpickle = join(outprocessingdir, f'{star_id}_merged_pbls_periodogram.pkl')
+    outpickle = join(outprocessingdir, f'{star_id}_merged_pbls_periodogram_iter{iter_ix}.pkl')
     with open(outpickle, 'wb') as f:
         pickle.dump(result, f)
     print(f"Wrote merged periodogram to {outpickle}")
 
-    outpng = join(outprocessingdir, f'{star_id}_merged_pbls_periodogram.png')
-    set_style("science")
+    outpng = join(outprocessingdir, f'{star_id}_merged_pbls_periodogram_iter{iter_ix}.png')
     fig, ax = plt.subplots(figsize=(10, 6))
     plot_periodogram(ax, periods, powers, title=f'PBLS {star_id}')
     fig.savefig(outpng, dpi=300, bbox_inches='tight')
