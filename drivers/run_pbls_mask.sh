88d75753623d438641a19432be227194dd10c2a8
lgbouma <bouma.luke@gmail.com>
2025-07-04 14:12:57 -0500

    housekeeping

--- drivers/run_pbls_mask.sh
+++ drivers/run_pbls_mask.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#arguments           = $(star_id) $(iter_ix) $(threshold) $(maxiter)
+# Argument order: $(star_id) $(iter_ix) $(threshold) $(maxiter)
 
 echo 'Running the masking locally...'
 
@@ -12,14 +12,14 @@ cp /ospool/ap21/data/ekul/Kepler/$1.tar.gz .
 
 # TODO TODO TODO FIXME LEFT OFF HERE!
 
-IMAGE=/ospool/ap21/data/ekul/python_311_f3c839.sif
+IMAGE=/ospool/ap21/data/ekul/python_311_3f829b.sif
 
 apptainer exec "${IMAGE}" python run_pbls_mask.py $1 $2 $3 $4
-
-# Output masked CSV light curve file is written by run_pbls_mask.py to top level
-# directory for condor to return.  Nothing required here, but remap in .sub
-# script.
 
 # Clean copied scratch.
 rm ./$1_merged_pbls_periodogram_iter$2.pkl
 rm ./$1.tar.gz
+
+# Move the masked light curve to the results directory.
+# because transfer_output_remaps doesn't work for local universe.
+mv $1_masked_lightcurve_iter$2.csv /ospool/ap21/data/ekul/pbls_results/PROCESSING/masked_lightcurves/$1_masked_lightcurve_iter$2.csv
