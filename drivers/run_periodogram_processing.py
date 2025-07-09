90309d0fa2e3f8135f5cd7dc1e9af703bd0ec828
lgbouma <bouma.luke@gmail.com>
2025-07-09 13:25:37 -0700

    [wip] osg: stage periodogram postprocessing

---
+++ drivers/run_periodogram_processing.py
@@ -0,0 +1,159 @@
+#############
+## LOGGING ##
+#############
+import logging
+from pbls import log_sub, log_fmt, log_date_fmt
+
+DEBUG = False
+if DEBUG:
+    level = logging.DEBUG
+else:
+    level = logging.INFO
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(
+    level=level,
+    style=log_sub,
+    format=log_fmt,
+    datefmt=log_date_fmt,
+    force=True
+)
+
+LOGDEBUG = LOGGER.debug
+LOGINFO = LOGGER.info
+LOGWARNING = LOGGER.warning
+LOGERROR = LOGGER.error
+LOGEXCEPTION = LOGGER.exception
+
+#############
+## IMPORTS ##
+#############
+import os, sys, socket, pickle
+from os.path import join
+import numpy as np, pandas as pd
+
+from pbls.getters import get_OSG_local_fits_lightcurve, get_OSG_local_csv_lightcurve
+from pbls.lc_processing import preprocess_lightcurve, get_LS_Prot
+from pbls.periodogram_processing import iterative_gaussian_whitening, trimmean_whitening
+from pbls.pbls import pbls_search
+from pbls.visualization import plot_summary_figure
+
+def main():
+
+    star_id = sys.argv[1]
+    iter_ix = int(sys.argv[2])
+
+    # HARD-CODED PARAMETERS (for now)
+    method = 'trimmean'  # or 'itergaussian'
+    poly_order = 3
+    durations_hr = np.array([1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6])
+
+    LOGINFO(42*'-')
+    LOGINFO('Starting run_periodogram_processing.py with')
+    LOGINFO(f'star_id = {star_id} (type={type(star_id)})')
+    LOGINFO(f'iter_ix = {iter_ix} (type={type(iter_ix)})')
+
+    LOGINFO(f'{os.listdir("./")}')
+
+    hostname = socket.gethostname()
+
+    if 'kplr' in star_id or 'Kepler-' in star_id:
+        mission = 'Kepler'
+    elif 'tess' in star_id or 'TOI-' in star_id:
+        mission = 'TESS'
+    elif '_k2_' in star_id or 'K2-' in star_id:
+        mission = 'K2'
+
+    ##########################################
+       
+    # Get periodogram information
+    pg_csv_path = f"./{star_id}_merged_pbls_periodogram_iter{iter_ix}.csv"
+    df = pd.read_csv(pg_csv_path)
+    x, y = df['period'].values, df['power'].values
+    x_start, y_start = x.copy(), y.copy()
+
+    # Load time and flux
+    ########################################################################
+    # vvv BEGIN EXACT DUPLICATE of code from pbls/pbls_chunk_pipeline.py vvv
+    # [Otherwise masks + PBLS iterations wouldn't apply to exact same LCs.]
+    if iter_ix == 0:
+        if hostname in ['wh1', 'wh2', 'wh3', 'marduk.local']:
+            raise NotImplementedError
+        else:
+            datas, hdrs = get_OSG_local_fits_lightcurve(star_id)
+            N_lcfiles = len(datas)
+            LOGINFO(f"{star_id}: {N_lcfiles} light curves found.")
+            time, flux = preprocess_lightcurve(datas, hdrs, mission)
+    else:
+        # Load the masked light curve made by mask.sub last iteration.
+        time, flux = get_OSG_local_csv_lightcurve(star_id, iter_ix=iter_ix-1)
+    # ^^^ END EXACT DUPLICATE ^^^
+    ########################################################################
+
+    sel = np.isfinite(time) & np.isfinite(flux)
+    ftime, fflux = time[sel], flux[sel]
+    LS_Prot = get_LS_Prot(ftime, fflux)
+
+    if method == 'itergaussian':
+        pg_results = iterative_gaussian_whitening(x, y)
+    elif method == 'trimmean':
+        pg_results = trimmean_whitening(x, y, Prot=LS_Prot)
+    else:
+        raise ValueError(f"Unknown method: {method}. Use 'itergaussian' or 'trimmean'.")
+
+    # Read the whitened periodogram results; recalculate best-fit model params
+    max_key = max(np.array(list(pg_results.keys())))
+    peak_period = pg_results[max_key]['peak_period']
+    res = pbls_search(ftime, fflux, np.array([peak_period]), durations_hr, poly_order)
+
+    # Extract period-level max SNR and corresponding best model params
+    power0 = res['power'][0]
+    bp = res['best_params']
+    bm = res['best_model']
+
+    # Post-processed PBLS power
+    post_power = pg_results[max_key]['residual']
+
+    # Cache post-processed periodogram results to pickle and CSV files.
+    assert len(x) == len(post_power), "Length of periods and post_power must match."
+
+    # Define directories for reading merged periodograms
+    if hostname in ['wh1', 'wh2', 'wh3']:
+        outprocessingdir = f'/ar0/PROCESSING/merged_periodograms'
+    elif 'osg' in hostname:
+        # Pre-tarred light curves are passed via HTCondor mask.sub to CWD.
+        outprocessingdir = "./"
+    else:
+        raise NotImplementedError
+
+    result = {
+        'best_params': bp,
+        'power': post_power,
+        'periods': x,
+        'best_model': bm,
+    }
+
+    outpickle = join(outprocessingdir, f'{star_id}_merged_postprocessed_pbls_periodogram_iter{iter_ix}.pkl')
+    with open(outpickle, 'wb') as f:
+        pickle.dump(result, f)
+    LOGINFO(f"Wrote merged post-processed periodogram to {outpickle}")
+
+    lines = ['period,power\n'] + [f"{period},{power}\n" for period, power in zip(x, post_power)]
+    outcsv = join(outprocessingdir, f'{star_id}_merged_postprocessed_pbls_periodogram_iter{iter_ix}.csv')
+    with open(outcsv, 'w') as f:
+        f.writelines(lines)
+    LOGINFO(f"Wrote merged post-processed periodogram to {outcsv}")
+    LOGINFO(bp)
+
+    # Make summary figure plot (based on the peak found after whitening)
+    fig = plot_summary_figure(time, flux, x_start, y_start, bp, bm, post_power=post_power)
+
+    plot_path = join(
+        outprocessingdir, 'viz',
+        f"{star_id}_pbls_pgproc{method}_iter{iter_ix}.png"
+    )
+    fig.savefig(plot_path, dpi=300, bbox_inches="tight")
+    LOGINFO(f"Saved summary figure to {plot_path}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ drivers/run_periodogram_processing.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Args: $(star_id) $(iter_ix)
+# Example:
+#   ./run_periodogram_processing.sh kplr006184894 0
+
+echo 'Running the periodogram post-processing locally...'
+
+# Transfer merged periodogram to where apptainer will see it.  Made by merge.sub.
+cp /ospool/ap21/data/ekul/pbls_results/PROCESSING/merged_periodograms/$1_merged_pbls_periodogram_iter$2.csv .
+
+# Transfer the appropriate stage of light curve 
+if [ "$2" -eq 0 ]; then
+    cp /ospool/ap21/data/ekul/Kepler/"$1".tar.gz .
+else
+    prev_iter=$(( $2 - 1 ))
+    cp /ospool/ap21/data/ekul/pbls_results/PROCESSING/masked_lightcurves/"${1}_masked_lightcurve_iter${prev_iter}.csv" .
+fi
+
+# Run the periodogram post-processing to identify sharp peaks.
+IMAGE=/ospool/ap21/data/ekul/python_311_70eb44.sif
+apptainer exec "${IMAGE}" python run_periodogram_processing.py $1 $2
+
+# Move the newly-made processing periodogram to the results directory.
+# because transfer_output_remaps doesn't work for local universe.
+mv $1_merged_postprocessed_pbls_periodogram_iter$2.pkl /ospool/ap21/data/ekul/pbls_results/PROCESSING/merged_periodograms/$1_merged_postprocessed_pbls_periodogram_iter$2.pkl
+mv $1_merged_postprocessed_pbls_periodogram_iter$2.csv /ospool/ap21/data/ekul/pbls_results/PROCESSING/merged_periodograms/$1_merged_postprocessed_pbls_periodogram_iter$2.csv
+mv $1_pbls_pgproc*_iter$2.png /ospool/ap21/data/ekul/pbls_results/PROCESSING/viz/.
+
+# Clean copied scratch.
+rm ./$1_merged_pbls_periodogram_iter$2.csv
+rm ./$1.tar.gz
+if [ "$2" -gt 0 ]; then
+    prev_iter=$(( $2 - 1 ))
+    prev_file="${1}_masked_lightcurve_iter${prev_iter}.csv"
+    if [ -f "$prev_file" ]; then
+        rm "$prev_file"
+        echo "Removed (local) previous masked light curve: $prev_file"
+    fi
+fi
